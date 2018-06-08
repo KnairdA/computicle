52a78e8bcb8efe9b825c3d42f16e79af7b8477f4
Adrian Kummerlaender <knairda@t-online.de>
2018-06-08 14:14:40 +0200

    Update NIX expression

--- shell.nix
+++ shell.nix
@@ -1,12 +1,18 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
-  name = "env";
+  name = "computicle-env";
   env = buildEnv { name = name; paths = buildInputs; };
+
   buildInputs = [
+    fish
     git cmake gcc gdb cgdb
     glfw3
     glew
     glm
   ];
+
+  shellHook = ''
+    export NIX_SHELL_NAME="${name}"
+  '';
 }
